36efaf3b36f75b54fe73d3f0b23af06ac9ace2c7
Justin Scott <jjustin@gmail.com>
2013-07-21 15:41:59 -0700

    added site to repo

---
+++ protected/views/site/index.php
@@ -0,0 +1,17 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<p>This site is under construction. Here I plan on being able to take a different approacher<br />
+to supporting user from home to businesses with everything technology. Example you have a question<br />
+about the best Blue Ray system for your home or board room. Once purchased would like to have it installed.<br />
+Or if you would like to buy the best computer for your needs then would like to have the bloatware removed after<br />
+purchase.</p>
+
+<p>
+	If you have any feed back or contributions please email me @ <a href="emailto:justinscott@tallguysolutions.com">Justin Scott at Tall Guy Solutions dot Com</a>
+
